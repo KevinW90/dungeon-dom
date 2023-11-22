e06fca7752f55e50a39008e2ce4c54a3a138266b
KevinW90 <kevinjwilliams76@gmail.com>
2023-11-22 15:28:47 -0500

    player stats display on screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let player = {
+		name: 'Player',
+		hp: 10,
+		maxHp: 10,
+		atk: 3,
+		def: 2
+	};
+</script>
+
+<!-- display all the player's stats -->
+<h1>{player.name}</h1>
+<p>HP: {player.hp}/{player.maxHp}</p>
+<p>ATK: {player.atk}</p>
+<p>DEF: {player.def}</p>
