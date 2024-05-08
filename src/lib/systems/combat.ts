d71d53f8b6c3970ad27eed909268f5ea24cc6069
KevinW90 <kevinjwilliams76@gmail.com>
2024-05-08 10:23:34 -0400

    combat system: weapons break

--- src/lib/systems/combat.ts
+++ src/lib/systems/combat.ts
@@ -1,10 +1,10 @@
 import { game } from '$lib/stores';
-import type { Character } from '$lib/types';
+import type { Character, Weapon } from '$lib/types';
 import { calculateAttackPoints, calculateDefensePoints } from '$lib/utils';
 import { get } from 'svelte/store';
 
 export function attack(attacker: Character, defender: Character): void {
-	console.log(`${attacker.name} attacks ${defender.name}`);
+	console.log(`${attacker.name} attacks ${defender.name} with a ${attacker.weapon?.name}!`);
 
 	// damage is at least 1
 	const damage = Math.max(1, calculateAttackPoints(attacker) - calculateDefensePoints(defender));
@@ -13,15 +13,12 @@ export function attack(attacker: Character, defender: Character): void {
 	// check the current weapon durability
 	if (!attacker.weapon?.durability) return;
 
-	// reduce the weapon durability
+	// handle weapon durability
 	attacker.weapon.durability -= 1;
-	// TODO: `breakWeapon` function
-	if (attacker.weapon?.durability > 0) return;
-	console.log(`${attacker.name}'s ${attacker.weapon.name} breaks!`);
-	attacker.weapon = null;
-	// equip the basic weapon
-	const basicWeapon = attacker.inventory.find((i) => i?.type === 'basic')!;
-	attacker.weapon = basicWeapon;
+	if (attacker.weapon.durability > 0) return;
+	breakWeapon(attacker, attacker.weapon);
+
+	// no game update needed, takeDamage always fires an update
 }
 
 export function takeDamage(defender: Character, damage: number): void {
@@ -43,4 +40,22 @@ export function takeDamage(defender: Character, damage: number): void {
 	gameCopy.room.tiles[ndx].content = newDefenderStats.hp <= 0 ? null : newDefenderStats;
 	// Update the game store with the modified game object
 	game.update((g) => ({ ...g, ...gameCopy }));
+}
+
+function breakWeapon(character: Character, weapon: Weapon): void {
+	console.log(`${character.name}'s ${weapon.name} breaks!`);
+	character.weapon = null;
+	// equip the basic weapon (hero) or random weapon (enemy)
+	if (character.type === 'hero') {
+		// TODO: equipBasicWeapon(character);
+		const basicWeapon = character.inventory.find((i) => i?.type === 'basic')!;
+		character.weapon = basicWeapon;
+	} else {
+		// TODO: equipRandomWeapon(character);
+		const weapons = character.inventory.filter((i) => i?.damage > 0);
+		if (weapons.length === 0) return;
+
+		const randomWeapon = weapons[Math.floor(Math.random() * weapons.length)];
+		character.weapon = randomWeapon;
+	}
 }
