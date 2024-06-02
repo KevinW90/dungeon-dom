e8cfdda9e173b54f409e7ab83cb00a0651cf1630
KevinW90 <kevinjwilliams76@gmail.com>
2024-06-02 11:45:39 -0400

    display room tiles on screen

--- src/lib/systems/room.ts
+++ src/lib/systems/room.ts
@@ -2,16 +2,32 @@
 
 import { writable, type Writable } from 'svelte/store';
 
+type Tile = {
+	id: number;
+};
+
 type Room = {
 	id: string;
+	width: number;
+	height: number;
+	tiles: Tile[];
 };
 
 export const currentRoom: Writable<null | Room> = writable(null);
 
 function newRoom(width: number = 5, height: number = 5) {
 	currentRoom.set({
-		id: Math.random().toString(36).substring(2, 9)
+		id: Math.random().toString(36).substring(2, 9),
+		width,
+		height,
+		tiles: tiles(width, height)
 	});
+}
+
+function tiles(w: number, h: number) {
+	return Array.from({ length: w * h }).map((_, i) => ({
+		id: i
+	}));
 }
 
 export default {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,33 @@
 <main>
 	<h1>{$currentRoom?.id}</h1>
 	<button on:click={() => room.new()}>Enter new Room</button>
+
+	{#if $currentRoom}
+		<div class="map" style="--width:{$currentRoom.width};--height:{$currentRoom.height}">
+			{#each $currentRoom?.tiles as tile}
+				<div class="tile">{tile.id}</div>
+			{/each}
+		</div>
+	{/if}
 </main>
+
+<style>
+	.map {
+		display: grid;
+		grid-template-columns: repeat(var(--width), 1fr);
+		grid-template-rows: repeat(var(--height), 1fr);
+	}
+
+	.tile {
+		text-align: center;
+		border: 1px solid black;
+	}
+
+	.tile:nth-child(odd) {
+		background-color: #bebebe;
+	}
+
+	.tile:nth-child(even) {
+		background-color: #646464;
+	}
+</style>
